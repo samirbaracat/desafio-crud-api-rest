2ee3efbd895e725aac979f7b30150706b1b7d790
samirbaracat <samirbaracat@gmail.com>
2023-06-11 11:15:32 -0400

    CRUD de Clientes

---
+++ src/main/java/com/devsuperior/crudclientes/dto/ClientDTO.java
@@ -0,0 +1,60 @@
+package com.devsuperior.crudclientes.dto;
+
+import com.devsuperior.crudclientes.entities.Client;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+    private Long id;
+    @NotBlank(message = "Nome nao pode estar vazio")
+    private String name;
+    private String cpf;
+    private Double income;
+    @PastOrPresent(message = "A Data de Nascimento nao pode ser data futura")
+    private LocalDate birthDate;
+    private Integer children;
+
+    public ClientDTO(Long id, String name, String cpf, Double income, LocalDate birthDate, Integer children) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
+
+    public ClientDTO(Client entity) {
+        id = entity.getId();
+        name = entity.getName();
+        cpf = entity.getCpf();
+        income = entity.getIncome();
+        birthDate = entity.getBirthDate();
+        children = entity.getChildren();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Felipe Kaique Alves', '38692732087', 2500.0, '1977-03-05', 2);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Carlos Manuel Rocha', '77292841546', 3350.0, '1964-03-02', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Ana Mariah Alana Lopes', '88156144350', 7800.0, '1980-03-10', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Nina Fabiana Vieira', '99363340945', 12800.0, '1977-02-27', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Nair Olivia Moura', '71088177131', 1900.0, '1987-01-21', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Raimunda Lorena Ester Mendes', '59124561703', 14150.0, '1964-05-01', 1);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Benedito Manoel João Gomes', '10530410397', 10900.0, '1995-06-02', 3);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Benício Diogo Gael da Mata', '22889781046', 5400.0, '1945-03-06', 5);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Alana Ana Tatiane Lima', '74444997486', 4000.0, '1998-01-24', 0);
+INSERT INTO tb_client(name, cpf, income, birth_date, children) VALUES ('Cláudio Cauê Silva', '50028674219', 9470.0, '1965-02-15', 4);
